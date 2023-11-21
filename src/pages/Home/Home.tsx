1098e4e9594e086659928a8affb689a19e5ed14a
brother1-4752 <guddlf4752@gmail.com>
2023-11-22 04:52:43 +0900

    feat: 홈 네비게이션 화면 구현

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,30 @@
+const Home = () => {
+  const mockSellerId = 123123
+  return (
+    <div>
+      <h1>Home</h1>
+
+      <ul>
+        <li>
+          <a href="/sign-in">로그인</a>
+        </li>
+        <li>
+          <a href="/sign-up">회원가입</a>
+        </li>
+        <li>
+          <a href="/biz-center">비즈센터</a>
+          <ul>
+            <li>
+              <a href={`/biz-center/bizm/seller/${mockSellerId}`}>카카오 비즈엠 셀러({mockSellerId})</a>
+            </li>
+            <li>
+              <a href="/biz-center/bizm/admin">카카오 비즈엠 어드민</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Home
