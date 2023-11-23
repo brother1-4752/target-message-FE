e00f695de0e40cdd518e48649597d0d34fb34691
brother1-4752 <guddlf4752@gmail.com>
2023-11-24 05:28:37 +0900

    style: 글로벌스타일로 웹폰트 적용

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,6 +1,37 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 import reset from 'styled-reset'
+
+const fontFaces = css`
+  @font-face {
+    font-family: 'SpoqaHanSansNeo';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2108@1.1/SpoqaHanSansNeo-Regular.woff') format('woff');
+    font-weight: 400;
+  }
+
+  @font-face {
+    font-family: 'SpoqaHanSansNeoB';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2108@1.1/SpoqaHanSansNeo-Bold.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'SpoqaHanSansNeoT';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2108@1.1/SpoqaHanSansNeo-Thin.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'SpoqaHanSansNeoM';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2108@1.1/SpoqaHanSansNeo-Medium.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'SpoqaHanSansNeoL';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2108@1.1/SpoqaHanSansNeo-Light.woff') format('woff');
+  }
+`
 
 export const GlobalStyle = createGlobalStyle`
     ${reset}
+    
+    ${fontFaces}
+    
 `
