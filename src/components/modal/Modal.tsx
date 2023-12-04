e41b474adcfbe6f2443ebdb2cbb5da92179274da
brother1-4752 <guddlf4752@gmail.com>
2023-12-04 16:34:15 +0900

    feat:  모달창 컴포넌트 인터페이스 변경,비즈엠 API 호출시 필요한 데이터 state로 관리  / no image icon 추가
    - image : image url, image link
    - button : name, type, url_mobile
    - reserved_date
    - message

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,11 +1,20 @@
 import { createPortal } from 'react-dom'
 import useLockBodyScroll from '../../hooks/useLockBodyScroll'
+import { useEffect } from 'react'
+import styled from 'styled-components'
 
-interface Image {
-  image_type: 'general' | 'wide'
+export type ImageType = 'general' | 'wide'
+
+export type ImageTypeProps = {
+  [key in ImageType]: ImageCardProps[]
+}
+
+export interface ImageCardProps {
+  id: number
   image_url: string
   image_name: string
-  checked: boolean
+  image_link: string
+  checked?: boolean
 }
 
 interface ModalProps {
@@ -17,12 +26,59 @@ const Modal = ({ onClose, children }: ModalProps) => {
   const modalRoot = document.getElementById('modal-root')
   useLockBodyScroll()
 
+  useEffect(() => {
+    document.body.style.overflow = 'hidden'
+
+    return () => {
+      document.body.style.overflow = 'unset'
+    }
+  }, [])
+
   if (!modalRoot) return null
 
   return createPortal(
-    <div className="modal__overlay" onClick={onClose}>
-      <div className="modal__content">{children}</div>
-    </div>,
+    <ModalOverlay className="modal__overlay">
+      <ModalContent className="modal__content">{children}</ModalContent>
+    </ModalOverlay>,
     modalRoot
   )
 }
+
+export default Modal
+
+const ModalOverlay = styled.div`
+  z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+`
+
+const ModalContent = styled.div`
+  width: 500px;
+  height: 441px;
+  display: flex;
+  flex-direction: column;
+  padding: 32px 24px 32px 24px;
+  gap: 32px;
+  border-radius: 24px;
+  background-color: rgba(255, 255, 255, 1);
+
+  .modal__wrapper {
+    width: 100%;
+    height: 80%;
+    .modal__header {
+      display: flex;
+      width: 100%;
+      justify-content: space-between;
+    }
+  }
+`
--- src/pages/Sending/BizM/BizMSeller.tsx
+++ src/pages/Sending/BizM/BizMSeller.tsx
@@ -1,11 +1,126 @@
+/* eslint-disable no-constant-condition */
 import styled from 'styled-components'
-import { HomeIcon, SendingIcon } from '../../../components/common/Icons/Icons'
-import { useState } from 'react'
+import { HomeIcon, NoImageIcon, SendingIcon } from '../../../components/common/Icons/Icons'
+import { FormEvent, useState } from 'react'
+import Modal, { ImageType, ImageTypeProps } from '../../../components/modal/Modal'
+
+const MockImageCardList: ImageTypeProps = {
+  general: [
+    {
+      id: 1,
+      image_url: 'https://picsum.photos/50/50',
+      image_name: '일반 이미지1',
+      image_link: 'https://brandi.co.kr',
+    },
+    {
+      id: 2,
+      image_url: 'https://picsum.photos/50/50',
+      image_name: '일반 이미지2',
+      image_link: 'https://brandi.co.kr',
+    },
+    {
+      id: 3,
+      image_url: 'https://picsum.photos/50/50',
+      image_name: '일반 이미지3',
+      image_link: 'https://brandi.co.kr',
+    },
+    {
+      id: 4,
+      image_url: 'https://picsum.photos/50/50',
+      image_name: '일반 이미지4',
+      image_link: 'https://brandi.co.kr',
+    },
+    {
+      id: 5,
+      image_url: 'https://picsum.photos/50/50',
+      image_name: '일반 이미지5',
+      image_link: 'https://brandi.co.kr',
+    },
+    {
+      id: 6,
+      image_url: 'https://picsum.photos/50/50',
+      image_name: '일반 이미지6',
+      image_link: 'https://brandi.co.kr',
+    },
+    {
+      id: 7,
+      image_url: 'https://picsum.photos/50/50',
+      image_name: '일반 이미지7',
+      image_link: 'https://brandi.co.kr',
+    },
+    {
+      id: 8,
+      image_url: 'https://picsum.photos/50/50',
+      image_name: '일반 이미지8',
+      image_link: 'https://brandi.co.kr',
+    },
+  ],
+
+  wide: [
+    {
+      id: 1,
+      image_url: 'https://picsum.photos/80/50',
+      image_name: '와이드 이미지1',
+      image_link: 'https://brandi.co.kr',
+    },
+    {
+      id: 2,
+      image_url: 'https://picsum.photos/80/50',
+      image_name: '와이드 이미지2',
+      image_link: 'https://brandi.co.kr',
+    },
+    {
+      id: 3,
+      image_url: 'https://picsum.photos/80/50',
+      image_name: '와이드 이미지3',
+      image_link: 'https://brandi.co.kr',
+    },
+    {
+      id: 4,
+      image_url: 'https://picsum.photos/80/50',
+      image_name: '와이드 이미지4',
+      image_link: 'https://brandi.co.kr',
+    },
+    {
+      id: 5,
+      image_url: 'https://picsum.photos/80/50',
+      image_name: '와이드 이미지5',
+      image_link: 'https://brandi.co.kr',
+    },
+    {
+      id: 6,
+      image_url: 'https://picsum.photos/80/50',
+      image_name: '와이드 이미지6',
+      image_link: 'https://brandi.co.kr',
+    },
+    {
+      id: 7,
+      image_url: 'https://picsum.photos/80/50',
+      image_name: '와이드 이미지7',
+      image_link: 'https://brandi.co.kr',
+    },
+    {
+      id: 8,
+      image_url: 'https://picsum.photos/80/50',
+      image_name: '와이드 이미지8',
+      image_link: 'https://brandi.co.kr',
+    },
+  ],
+}
 
 const BizMSeller = () => {
+  //form-data input value
   const [btnUrl, setBtnUrl] = useState<string>('')
   const [btnName, setBtnName] = useState<string>('')
   const [date, setDate] = useState<string>('')
+  const [msg, setMsg] = useState<string>('')
+
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+  const [imageType, setImageType] = useState<ImageType>('general')
+  const [imageUrl, setImageUrl] = useState<string>('')
+  const [test, setTest] = useState<string>('')
+
+  console.log(imageUrl.length > 0 ? 'true' : 'false')
 
   const onChangeBtnUrlHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setBtnUrl(event.target.value)
@@ -15,11 +130,55 @@ const BizMSeller = () => {
     setBtnName(event.target.value)
   }
 
+  const onChangeMsg = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMsg(event.target.value)
+  }
+
   const onChangeDateHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setDate(event.target.value)
   }
 
-  const openImageSelectionModal = (event: React.FormEvent<HTMLButtonElement>) => {}
+  const openImageSelectionModal = (event: React.FormEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    setIsModalOpen((prev) => !prev)
+  }
+
+  const onChangeImageTypeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setImageType(event.target.value as ImageType)
+  }
+
+  const onClickImageHandler = (event: React.FormEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    //TODO: 이미지 아이템 중 src가 일치하는 것을 찾아서 해당 이미지를 미리보기에 띄워줘야 함
+    console.log(imageUrl)
+    console.log(imageType)
+    setIsModalOpen(false)
+  }
+
+  const onCheckImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // event.preventDefault()
+    const image_data = event.currentTarget.dataset.imagedata ?? ''
+    const [image_url, image_name] = image_data?.split('+') ?? ['', '', '']
+    console.log(image_name)
+
+    setImageUrl(image_url)
+    setTest(image_data)
+  }
+
+  const onsendingClick = (event: FormEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+
+    //버튼 데이터 리스트는 하나하나 onChange로 데이터 관리 X, 리스트째로 관리해야 함
+    console.log(btnUrl)
+    console.log(btnName)
+    console.log(date)
+    console.log(msg)
+    console.log(imageUrl)
+  }
+
+  const onDeleteClick = (event: FormEvent<HTMLButtonElement>) => {
+    console.dir(event.currentTarget.parentElement)
+  }
 
   return (
     <StyledBizMSeller>
@@ -35,7 +194,7 @@ const BizMSeller = () => {
           <div className="image__container">
             <label className="image__title">이미지 설정</label>
             <div className="image__description">
-              <div className="preview__img"></div>
+              {imageUrl.length === 0 ? <NoImageIcon /> : <PreviewImage image_url={imageUrl}></PreviewImage>}
               <div className="image__settings">
                 <div>
                   <button onClick={openImageSelectionModal}>
@@ -75,42 +234,42 @@ const BizMSeller = () => {
                 </tr>
               </thead>
               <tbody>
+                {/* TODO: 아래 버튼데이터리스트 null없이 모든 필드 필수값으로 채워지면, 
+                각 객체마다 데이터 묶어서 전송 */}
                 {[
                   { id: 1, type: 'text', name: '버튼1', url: 'http://example1' },
                   { id: 2, type: 'wl', name: '버튼2', url: 'http://example2' },
-                  { id: 3, type: null, name: null, url: null },
+                  // { id: 3, type: null, name: null, url: null },
                 ].map((item, index) => (
                   <tr key={index}>
                     <td>{item.id}</td>
                     <td>
-                      <select>
+                      <select defaultValue={item.type ?? ''} required>
                         <option value="">선택</option>
-                        <option value="text" selected={item.type === 'text'}>
-                          텍스트
-                        </option>
-                        <option value="wl" selected={item.type === 'wl'}>
-                          웹링크
-                        </option>
+                        <option value="text">텍스트</option>
+                        <option value="wl">웹링크</option>
                       </select>
                     </td>
                     <td>
                       <input
                         type="text"
                         placeholder="버튼명"
-                        value={item.name ?? undefined}
+                        defaultValue={item.name}
                         onChange={onChangeBtnNameHandler}
+                        required
                       />
                     </td>
                     <td>
                       <input
                         type="text"
                         placeholder="버튼 url"
-                        value={item.url ?? undefined}
+                        defaultValue={item.url}
                         onChange={onChangeBtnUrlHandler}
+                        required
                       />
                     </td>
                     <td>
-                      <button>-</button>
+                      <button onClick={onDeleteClick}>-</button>
                     </td>
                   </tr>
                 ))}
@@ -122,19 +281,87 @@ const BizMSeller = () => {
             </div>
           </div>
 
+          <div className="message__container">
+            <input type="text" placeholder="기본문구 입력..." onChange={onChangeMsg} />
+          </div>
+
           <div className="sending__container">
             <div className="sending__title">발송일자 설정</div>
             <input type="date" onChange={onChangeDateHandler} />
           </div>
 
-          <button>발송 예약</button>
+          <button onClick={onsendingClick}>발송 예약</button>
         </form>
       </div>
+
+      {isModalOpen && (
+        <Modal onClose={() => setIsModalOpen((prev) => !prev)}>
+          <div className="modal__wrapper">
+            <header className="modal__header">
+              <h1>이미지 선택</h1>
+              <button onClick={() => setIsModalOpen(false)}>X</button>
+            </header>
+
+            <input
+              type="radio"
+              name="image_type"
+              id="general"
+              value="general"
+              defaultChecked
+              onChange={onChangeImageTypeHandler}
+            />
+            <label htmlFor="general">일반</label>
+            <input type="radio" name="image_type" id="wide" value="wide" onChange={onChangeImageTypeHandler} />
+            <label htmlFor="wide">와이드</label>
+            <IamgeCardWrapper>
+              {MockImageCardList[imageType].map((item) => (
+                <ImageCardBox key={item.id}>
+                  <img src={item.image_url} alt={item.image_name} />
+                  <p>
+                    <input
+                      type="checkbox"
+                      checked={test === `${item.image_url}+${item.image_name}`}
+                      data-imagedata={`${item.image_url}+${item.image_name}`}
+                      onChange={onCheckImage}
+                    />
+                    {item.image_name}
+                  </p>
+                </ImageCardBox>
+              ))}
+            </IamgeCardWrapper>
+          </div>
+          <div>
+            <button onClick={onClickImageHandler}>선택</button>
+            <button onClick={() => setIsModalOpen(false)}>취소</button>
+          </div>
+        </Modal>
+      )}
     </StyledBizMSeller>
   )
 }
 
 export default BizMSeller
+
+const PreviewImage = styled.div<{ image_url: string }>`
+  width: 100px;
+  height: 100px;
+  /* TODO: 초기 로딩시 No Image 렌더링 */
+  background-image: url(${({ image_url }) => image_url});
+  background-size: cover;
+`
+
+const IamgeCardWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 16px;
+`
+
+const ImageCardBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 16px;
+`
 
 const StyledBizMSeller = styled.div`
   padding: 32px 0 0 32px;
@@ -171,12 +398,6 @@ const StyledBizMSeller = styled.div`
       .image__description {
         display: flex;
         gap: 16px;
-      }
-
-      .preview__img {
-        width: 100px;
-        height: 100px;
-        background-color: green;
       }
 
       .image__settings {
