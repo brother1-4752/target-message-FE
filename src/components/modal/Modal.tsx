8a3c07549aa990dd3356d91a7d10e60e1b2af802
brother1-4752 <guddlf4752@gmail.com>
2023-12-04 16:41:22 +0900

    feat: 모달 컴포넌트 관심사 분리

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,7 +1,7 @@
 import { createPortal } from 'react-dom'
 import useLockBodyScroll from '../../hooks/useLockBodyScroll'
 import { useEffect } from 'react'
-import styled from 'styled-components'
+import { ModalContent, ModalOverlay } from './Modal.styled'
 
 export type ImageType = 'general' | 'wide'
 
@@ -18,11 +18,10 @@ export interface ImageCardProps {
 }
 
 interface ModalProps {
-  onClose: () => void
   children: React.ReactNode
 }
 
-const Modal = ({ onClose, children }: ModalProps) => {
+const Modal = ({ children }: ModalProps) => {
   const modalRoot = document.getElementById('modal-root')
   useLockBodyScroll()
 
@@ -37,48 +36,11 @@ const Modal = ({ onClose, children }: ModalProps) => {
   if (!modalRoot) return null
 
   return createPortal(
-    <ModalOverlay className="modal__overlay">
-      <ModalContent className="modal__content">{children}</ModalContent>
+    <ModalOverlay>
+      <ModalContent>{children}</ModalContent>
     </ModalOverlay>,
     modalRoot
   )
 }
 
 export default Modal
-
-const ModalOverlay = styled.div`
-  z-index: 10;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: fixed;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-`
-
-const ModalContent = styled.div`
-  width: 500px;
-  height: 441px;
-  display: flex;
-  flex-direction: column;
-  padding: 32px 24px 32px 24px;
-  gap: 32px;
-  border-radius: 24px;
-  background-color: rgba(255, 255, 255, 1);
-
-  .modal__wrapper {
-    width: 100%;
-    height: 80%;
-    .modal__header {
-      display: flex;
-      width: 100%;
-      justify-content: space-between;
-    }
-  }
-`
